a3f1af5da2dc0f35463808e0f78c658851431ec1
MrRisoni <vanguelisk@gmail.com>
2020-12-02 18:53:34 +0200

    1st commit

---
+++ retailshop/ProductCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace retailshop
+{
+    public class ProductCategory
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+    }
+}
---
+++ retailshop/Program.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace retailshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try
+            {
+                List<ProductCategory> categories = new List<ProductCategory>();
+                using (IDbConnection db = new System.Data.SqlClient.SqlConnection("Server=localhost;Database=multishopdb;Trusted_Connection=True;"))
+                {
+
+                    categories = db.Query<ProductCategory>("SELECT * FROM dbo.product_categories").ToList();
+                    Console.WriteLine(categories.ElementAt(0).title);
+                }
+                Console.WriteLine("Adieu World!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
