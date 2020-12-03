bbc21df59241c6b3695d99bf73a29ebf93185e0f
MrRisoni <vanguelisk@gmail.com>
2020-12-03 08:45:19 +0200

    query for campaign success

--- retailshop/Entities/Fetcher.cs
+++ retailshop/Entities/Fetcher.cs
@@ -32,6 +32,28 @@ namespace retailshop.Entities
                 Console.WriteLine(categories.ElementAt(0).title);
             }
         }
+
+        public static void getCampaignSuccess()
+            }
+    SELECT O.user_id, totalRecipients.totalUsers FROM newsletter_campaigns C
+JOIN newsletter_recipients R ON R.campaign_id = C.id
+JOIN orders O ON O.user_id = R.user_id
+JOIN order_items OI ON OI.order_id = O.id
+JOIN (
+  SELECT P.product_id FROM newsletter_campaigns C
+  JOIN newsletter_products P ON P.campaign_id = C.id
+  WHERE C.code = 'BLACK_FRIDAY_2020'
+) AS promo_products ON promo_products.product_id  = OI.product_id
+JOIN (
+ SELECT COUNT(R.user_id) AS totalUsers, C.id AS campaignId FROM newsletter_campaigns C
+  JOIN newsletter_recipients R ON R.campaign_id = C.id
+  WHERE C.code = 'BLACK_FRIDAY_2020'
+) AS totalRecipients ON totalRecipients.campaignId =  C.id
+WHERE C.code = 'BLACK_FRIDAY_2020'
+AND O.is_success= 1 AND O.is_void = 0
+AND O.created_at >= C.started_at
+AND O.created_at <= C.ended_at
+AND OI.is_void = 0 AND OI.is_refund = 0
     }
 
 
