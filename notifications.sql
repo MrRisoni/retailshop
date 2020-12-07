229356f085539b95f59ba9a679fb9cb595e08052
MrRisoni <vanguelisk@gmail.com>
2020-12-07 19:50:01 +0200

    un tk temp vars

--- notifications.sql
+++ notifications.sql
@@ -1,4 +1,9 @@
 DECLARE @routeIter varchar(6)
+DECLARE @statusIter varchar(2)
+
+DECLARE @unId bigint
+DECLARE @tkId bigint
+
 
 DECLARE routeCursor CURSOR FOR SELECT DISTINCT(route) FROM notification_history WHERE gdsPnr = 'PUSHKI'
 OPEN routeCursor;
@@ -7,7 +12,9 @@ OPEN routeCursor;
 FETCH NEXT FROM routeCursor INTO @routeIter;  
 WHILE @@FETCH_STATUS = 0  
 BEGIN
-	PRINT @routeIter;  
+	SELECT TOP 1 @routeIter,gdsChangedAt,status FROM notification_history WHERE route=@routeIter AND status = 'TK' ORDER BY gdsChangedAt DESC
+	SELECT TOP 1 @routeIter,gdsChangedAt,status FROM notification_history WHERE route=@routeIter AND status = 'UN' ORDER BY gdsChangedAt DESC
+
 	FETCH NEXT FROM routeCursor INTO @routeIter; 
 END;
 
