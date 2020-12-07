3715c5efbd998bf1773c63d9f6b8b29ffa859a42
MrRisoni <vanguelisk@gmail.com>
2020-12-07 20:26:35 +0200

    set two columns in vars

--- notifications.sql
+++ notifications.sql
@@ -1,19 +1,21 @@
 DECLARE @routeIter varchar(6)
 DECLARE @statusIter varchar(2)
 
-DECLARE @unId bigint
-DECLARE @tkId bigint
+DECLARE @unID bigint
+DECLARE @tkID bigint
+DECLARE @unTid datetime /* tid Danish for time */
+DECLARE @tkTid datetime
+DECLARE @validId bigint
 
-
-DECLARE routeCursor CURSOR FOR SELECT DISTINCT(route) FROM notification_history WHERE gdsPnr = 'PUSHKI'
+DECLARE routeCursor CURSOR FOR SELECT DISTINCT(route) FROM notification_history WHERE gdsPnr = 'PUSHKI' AND bookingId = 452875
 OPEN routeCursor;
 
 
 FETCH NEXT FROM routeCursor INTO @routeIter;  
 WHILE @@FETCH_STATUS = 0  
 BEGIN
-	SELECT TOP 1 @routeIter,gdsChangedAt,status FROM notification_history WHERE route=@routeIter AND status = 'TK' ORDER BY gdsChangedAt DESC
-	SELECT TOP 1 @routeIter,gdsChangedAt,status FROM notification_history WHERE route=@routeIter AND status = 'UN' ORDER BY gdsChangedAt DESC
+	SELECT TOP 1 @tkTid = gdsNotifiedAt,@tkID = id FROM notification_history WHERE route=@routeIter AND status = 'TK' ORDER BY gdsNotifiedAt DESC
+	SELECT TOP 1 @unTid = gdsNotifiedAt,@unID = id FROM notification_history WHERE route=@routeIter AND status = 'UN' ORDER BY gdsNotifiedAt DESC
 
 	FETCH NEXT FROM routeCursor INTO @routeIter; 
 END;
